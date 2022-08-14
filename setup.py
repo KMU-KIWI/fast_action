4ec4229b7574cbf31c8d2991be1e63231a98266a
urw7rs <urw.7rs@icloud.com>
2022-08-15 01:23:17 +0900

    remove torch

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ def main():
             "Operating System :: OS Independent",
         ],
         python_requires=">=3.7",
-        install_requires=[
-            "torch",
-        ],
+        install_requires=[],
     )
 
 
